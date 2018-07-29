cac44b2839bc2ac19274766caed4ac7908ad695e
RoySahana <sahanaroy.hk@gmail.com>
2018-07-29 20:18:05 +0800

    Html reuse and logo background modified.

---
+++ announcement.js
@@ -0,0 +1,8 @@
+document.write("          <h5 style=\"font-size: 24px;font-weight: bold;\">To open an acount</h5>");
+document.write("<ul>");
+document.write("  <li>Fill up the form.</li>");
+document.write("  <li>Concentrate on the details.</li>");
+document.write("  <li>Phone number should be correct.</li>");
+document.write("  <li>Email add should be correct.</li>");
+document.write("  <ul>");
+document.write("    <a href=\"#\" id=\"readMore\">Read More >>></a>");
