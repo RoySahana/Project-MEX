5fa15e5ed6c5ff5d0a79f96900592998788de5c5
RoySahana <sahanaroy.hk@gmail.com>
2018-07-30 02:22:57 +0800

    Containerized and validation

--- js/form.js
+++ js/form.js
@@ -6,11 +6,11 @@ document.write("  <input type=\"text\" placeholder=\"Your name\">");
 document.write("  <i class=\"fa fa-user fa-lg fa-fw\" aria-hidden=\"true\"></i>");
 document.write("</div>");
 document.write("<div class=\"inputWithIcon\">");
-document.write("  <input type=\"text\" placeholder=\"Phone Number\">");
+document.write("  <input type=\"number\" placeholder=\"Phone Number\">");
 document.write("  <i class=\"fa fa-phone fa-lg fa-fw\" aria-hidden=\"true\"></i>");
 document.write("</div>");
 document.write("<div class=\"inputWithIcon\">");
-document.write("  <input type=\"text\" placeholder=\"Email\">");
+document.write("  <input type=\"email\" placeholder=\"Email\">");
 document.write("  <i class=\"fa fa-envelope fa-lg fa-fw\" aria-hidden=\"true\"></i>");
 document.write("</div>");
 document.write("<div class=\"inputWithIcon\">");
@@ -18,8 +18,12 @@ document.write("  <input type=\"text\" placeholder=\"Address\">");
 document.write("  <i class=\"fa fa-envelope fa-lg fa-fw\" aria-hidden=\"true\"></i>");
 document.write("<div>");
 document.write("</div>");
-document.write("  <input type=\"submit\" value=\"SUBMIT\" class=\"submit\">");
+document.write("  <input type=\"button\" value=\"SUBMIT\" onclick=\"submitFunction()\" class=\"submit\">");
 document.write("</div>");
 document.write("<div>");
 document.write("  <p style=\"color:#FFFFFF;font-size:10px;\">Note:Please fill correct email and phone number<br> 1:Email add to receive the download link.<br> 2:Mobile number to receive verification info.</p>");
 document.write("</div>");
+
+function submitFunction() {
+    alert("The functionality is under maintenance! Please try again later");
+}
